65600112bd29d35f603ebde617469575f5ad1936
LontchiFrank <fodjof6@gmail.com>
2022-11-09 19:08:54 +0100

    so great

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const server = http.createServer(function (req, res) {
     res.write("</html>");
     return res.end();
   }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page </title></head>");
