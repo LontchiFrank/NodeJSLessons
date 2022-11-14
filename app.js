b3ba72f0a545cede31d95b9905cc4e7864160a74
LontchiFrank <fodjof6@gmail.com>
2022-11-14 20:30:26 +0100

    app use

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); //Allows the request to continue to the next middlewarein line
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware");
+  res.send(
+    "<form action='/product' method='POST'> <input type='text' name='title'><button type='submit'/>Add Product </button></form>"
+  );
 });
-app.use((req, res, next) => {
+
+app.use("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware");
   res.send("<h1>Hello from Express</h1>");
 });
