3d396a1d7a4b643b161bd8dfcc469bfe5fc92f2c
LontchiFrank <fodjof6@gmail.com>
2022-11-17 18:53:49 +0100

    created routes

--- app.js
+++ app.js
@@ -3,25 +3,14 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send(
-    "<form action='/product' method='POST'> <input type='text' name='title'><button type='submit'/>Add Product </button></form>"
-  );
-});
-
-app.use("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express</h1>");
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(8000);
