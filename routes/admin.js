5089090d4721e46971acb30c9539e09e629d81ce
LontchiFrank <fodjof6@gmail.com>
2022-11-19 19:04:34 +0100

    so great

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+//  /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   console.log("In another middleware");
   res.send(
-    "<form action='/product' method='POST'> <input type='text' name='title'><button type='submit'/>Add Product </button></form>"
+    "<form action='/admin /add-product' method='POST'> <input type='text' name='title'><button type='submit'/>Add Product </button></form>"
   );
 });
 
-router.use("/product", (req, res) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, nex) => {
   console.log(req.body);
   res.redirect("/");
 });
